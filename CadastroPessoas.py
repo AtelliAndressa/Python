47b45e61d336f08b257189078c640a4a1d0e6805
Andressa Atelli <andressaatelli.programador@gmail.com>
2020-11-14 20:03:59 -0300

    Programa de cadastro, codigo aberto e o executável

---
+++ CadastroPessoas.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject3',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Andressa',
+    author_email='',
+    description=''
+)
